81d35dcf561482392f8d3ddaa48a7018ff4865a3
Taisen Colcher <TaisenColcher@Gmail.com>
2023-04-19 16:27:18 -0400

    Create BoardWidgit

---
+++ Source/TicTacToe/BoardWidgit.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SpaceButton.h"
+#include "Blueprint/UserWidget.h"
+#include "BoardWidgit.generated.h"
+
+
+
+/**
+ * 
+ */
+UCLASS()
+class TICTACTOE_API UBoardWidgit : public UUserWidget
+{
+	GENERATED_BODY()
+
+	
+};
